3cf51e1528f98d9815e1101983c512c3de059d35
NathaliUlr <nathali.5@mail.ru>
2025-10-22 05:48:40 +0500

    Контрольная работа

---
+++ lesson_06K/test_01_form.py
@@ -0,0 +1,75 @@
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.common.by import By
+
+edge_driver_path = r"C:\Program Files (x86)\Microsoft\msedgedriver.exe"
+browser = webdriver.Edge(service=EdgeService(edge_driver_path))
+
+url = 'https://bonigarcia.dev/selenium-webdriver-java/data-types.html'
+browser.get(url)
+
+First_name = browser.find_element(By.NAME, 'first-name')
+First_name.send_keys('Иван')
+
+Last_name = browser.find_element(By.NAME, 'last-name')
+Last_name.send_keys('Петров')
+
+Address = browser.find_element(By.NAME, 'address')
+Address.send_keys('Ленина, 55-3')
+
+E_mail = browser.find_element(By.NAME, 'e-mail')
+E_mail.send_keys('test@skypro.com')
+
+Phone_number = browser.find_element(By.NAME, 'phone')
+Phone_number.send_keys('+7985899998787')
+
+Zip_code = browser.find_element(By.NAME, 'zip-code')
+Zip_code.send_keys('')
+
+City = browser.find_element(By.NAME, 'city')
+City.send_keys('Москва')
+
+Country = browser.find_element(By.NAME, 'country')
+Country.send_keys('Россия')
+
+Job_position = browser.find_element(By.NAME, 'job-position')
+Job_position.send_keys('QA')
+
+Company = browser.find_element(By.NAME, 'company')
+Company.send_keys('SkyPro')
+
+Submit_button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+Submit_button.click()
+
+red = 'rgb(245, 194, 199)'
+green = 'rgb(186, 219, 204)'
+
+zip_code_field = browser.find_element(By.ID, 'zip-code')
+
+zip_code_color = zip_code_field.value_of_css_property('border-color')
+
+
+def test_zip_code_field_red():
+    assert zip_code_color == red
+
+
+other_fields = [
+    browser.find_element(By.ID, 'first-name'),
+    browser.find_element(By.ID, 'last-name'),
+    browser.find_element(By.ID, 'address'),
+    browser.find_element(By.ID, 'e-mail'),
+    browser.find_element(By.ID, 'phone'),
+    browser.find_element(By.ID, 'city'),
+    browser.find_element(By.ID, 'country'),
+    browser.find_element(By.ID, 'job-position'),
+    browser.find_element(By.ID, 'company')
+]
+
+
+def test_other_fields_green():
+    fields_to_check = other_fields
+    for field in fields_to_check:
+        assert field.value_of_css_property('border-color') == green
+
+
+browser.quit
---
+++ lesson_06K/test_02_calc.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+service = Service(ChromeDriverManager().install())
+browser = webdriver.Chrome(service=service)
+
+url = 'https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html'
+browser.get(url)
+
+Delay = browser.find_element(By.ID, 'delay')
+Delay.clear()
+Delay.send_keys('45')
+
+Seven = browser.find_element(By.XPATH, ('//span[contains(text(), "7")]'))
+Seven.click()
+
+Plus = browser.find_element(By.XPATH, ('//span[contains(text(), "+")]'))
+Plus.click()
+
+Eight = browser.find_element(By.XPATH, ('//span[contains(text(), "8")]'))
+Eight.click()
+
+Still = browser.find_element(By.XPATH, ('//span[contains(text(), "=")]'))
+Still.click()
+
+field_solution = browser.find_element(By.CLASS_NAME, 'screen')
+
+
+def test_result_of_solving():
+    wait = WebDriverWait(browser, 45)
+    wait.until(EC.text_to_be_present_in_element(
+        (By.CLASS_NAME, 'screen'), '15'))
+    result = field_solution.text
+    assert result == '15'
+
+
+browser.quit
